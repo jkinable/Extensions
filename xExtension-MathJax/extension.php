6353d45d372c3b787b5119e64c8c8ecc990023a9
Joris Kinable <j.kinable@gmail.com>
2019-08-15 14:07:58 +0200

    updated meta data

--- xExtension-MathJax/extension.php
+++ xExtension-MathJax/extension.php
@@ -1,7 +1,8 @@
 <?php
 
 class MathJaxExtension extends Minz_Extension {
-	public function init() {
-		Minz_View::appendScript("https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-AMS_HTML",false,true,true);
-	}
+    public function init() {
+        header("Content-Security-Policy: script-src 'self' https://cdnjs.cloudflare.com/ajax/libs/mathjax/; script-src-elem 'self' https://cdnjs.cloudflare.com/ajax/libs/mathjax/");
+        Minz_View::appendScript("https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/latest.js?config=TeX-AMS_HTML",false,true,true);
+    }
 }
